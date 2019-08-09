b54cd54cb7222251f52689ce492bdce0d33dec9d
daniriian <daniriian@gmail.com>
2019-08-09 23:25:05 +0300

    Updates

---
+++ precache-manifest.5cc302ad2055c6bfbf6bdab97c019538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a8e38344d40aee14f825e163bd1471",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a39dd640b5216f46557b",
+    "url": "/monsters/static/css/main.26696a6e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a39dd640b5216f46557b",
+    "url": "/monsters/static/js/main.fd0048c9.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
